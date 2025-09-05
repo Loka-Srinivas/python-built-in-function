ca37836444981ff27171b1785a30933b9e2929c5
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-09-05 23:37:08 +0530

    Add files via upload

---
+++ String Bulit-in Function.py
@@ -0,0 +1,79 @@
+# a="Loka Srinivas"
+# if a.islower():
+#     print("upper")
+# else:
+#     print("lower")    
+
+
+
+# # Capitalize bulit-in function
+# a="loka srinivas"
+# a=a.capitalize()
+# print(a)
+
+
+
+# a="today i came to 4th floor"
+# print("upper case:-",a.upper())
+# print("lower case:-",a.lower())
+# print("swapcase:-",a.swapcase())
+# print("capitalize:-",a.capitalize())
+# print("title:-",a.title())
+# print(a)
+
+
+
+# a="     sravan       "    
+# a=a.strip()
+# print(a)
+
+
+
+# a="     sravan       "    
+# a=a.lstrip()
+# print(a)
+
+
+# rstrip Bulit in function gape right gape was full:-
+# a="     sravan       "    
+# a=a.rstrip()
+# print(a)
+
+
+# Character Change replace Function:-
+# a="iam a java trainer"
+# a=a.replace("java","python")
+# print(a)
+
+
+
+# Single  Character Change replace Function:-
+# a="iam a java trainer"
+# a=a.replace("a","s")
+# print(a)
+
+
+# Find Bulit-in Function:-
+# a="loka srinivas"
+# print(a.find("s"))
+
+
+
+# This also is the find bulit-in function:-
+# a="loka srinivas"
+# print(a.find("y"))
+
+
+
+
+
+# This is rfind Bulit-in Function:-
+# a="loka srinivas"
+# print(a.rfind("a"))
+
+
+
+
+# This is count bu;it-in Function:-
+# a="loka srinivasssssssssssss"
+# print(a.count("s"))
