f62e7eaaf48425f1ffe95e7b233818df56d04d2b
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-10-29 23:04:51 +0530

    Add files via upload

---
+++ Scoping.py
@@ -0,0 +1,54 @@
+# # Local variable:
+
+
+# def display():
+#     x=10
+#     print(x)
+    
+    
+# display()   
+
+# print(x)  this is gives error
+
+# # Global Varibles:
+    
+# gloVar=10
+# def display():
+#     print(gloVar)
+# display()
+# print(gloVar)        
+ 
+
+
+# # Non-Local/Enclosing: 
+
+
+      
+# def outfunc():
+#     var1=25
+#     def infunc():
+#         print(var1) 
+#     infunc()
+# outfunc()              
+     
+  
+  
+# # Bulit in 
+
+
+       
+# max=10
+# print(max)       
+
+
+
+# var1=25
+
+
+# def sample1():
+#     var1=22
+#     var1+=5
+#     print(var1)
+    
+# sample1()
+# print(var1)    
