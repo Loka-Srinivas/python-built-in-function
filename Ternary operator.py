b31a010f1c0167eb4d0967e21ddbceea148122f8
Mancha Loka Srinivas <lokasrinivas.mancha@gmail.com>
2025-10-31 23:45:58 +0530

    Add files via upload

---
+++ Ternary operator.py
@@ -0,0 +1,11 @@
+
+# age=int (input("enter your age:"))
+# if age>18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible for vote")
+
+
+
+# # Ternary Operator Example
+#     print('eligible for vote' if age>=18 else 'not eligible for vote')
